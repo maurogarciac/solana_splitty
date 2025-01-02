b970ee689b136878fa29a62d2b797b15746c5be9
Mauro Garcia Coto <maurogarciacoto@gmail.com>
2025-01-02 16:46:50 -0300

    fixed some things

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use teloxide::{
+    requests::Requester,
+    types::Message,
+    Bot,
+};
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    info!("Starting telegram bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
+}
